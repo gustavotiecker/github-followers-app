64aeec22f5abf798c60c8250de21906b92e6f420
Gustavo Tiecker <gustavotiecker@gmail.com>
2020-11-04 17:10:26 -0300

    creating GFButton and GFTextField

---
+++ GithubFollowers/GFButton.swift
@@ -0,0 +1,39 @@
+//
+//  GFButton.swift
+//  GithubFollowers
+//
+//  Created by Gustavo Tiecker on 04/11/20.
+//
+
+import UIKit
+
+class GFButton: UIButton {
+    
+    override init(frame: CGRect) {
+        // this is for initialize a generic button, building our button as a "child" of UIButton
+        super.init(frame: frame)
+        
+        // custom code here:
+        configure()
+    }
+    
+    // this is going to handle initialization via storyboard
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(backgroundColor: UIColor, title: String) {
+        super.init(frame: .zero)
+        self.backgroundColor = backgroundColor
+        self.setTitle(title, for: .normal)
+        configure()
+    }
+    
+    private func configure() {
+        layer.cornerRadius = 10
+        titleLabel?.textColor = .white
+        titleLabel?.font = UIFont.preferredFont(forTextStyle: .headline)
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+}
