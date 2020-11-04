6c3be9525824d287e8e17a9e46ccea4ce5866bfe
Gustavo Tiecker <gustavotiecker@gmail.com>
2020-11-03 23:37:39 -0300

    setting up the app navigation

--- GithubFollowers/FavoritesListVC.swift
+++ GithubFollowers/FavoritesListVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  FavoritesListVC.swift
 //  GithubFollowers
 //
 //  Created by Gustavo Tiecker on 03/11/20.
@@ -7,13 +7,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class FavoritesListVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBlue
     }
-
-
 }
-
---
+++ GithubFollowers/SearchVC.swift
@@ -0,0 +1,16 @@
+//
+//  SearchVC.swift
+//  GithubFollowers
+//
+//  Created by Gustavo Tiecker on 03/11/20.
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+    }
+}
