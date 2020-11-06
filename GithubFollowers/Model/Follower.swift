d0e96e2f65aa3480f50244cb918d2038310f7f2e
Gustavo Tiecker <gustavotiecker@gmail.com>
2020-11-06 16:56:30 -0300

    creating NetworkManager and the models for the data

---
+++ GithubFollowers/Model/Follower.swift
@@ -0,0 +1,13 @@
+//
+//  Follower.swift
+//  GithubFollowers
+//
+//  Created by Gustavo Tiecker on 06/11/20.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    var login: String
+    var avatarUrl: String
+}
---
+++ GithubFollowers/Model/User.swift
@@ -0,0 +1,22 @@
+//
+//  User.swift
+//  GithubFollowers
+//
+//  Created by Gustavo Tiecker on 06/11/20.
+//
+
+import Foundation
+
+struct User: Codable {
+    var login: String
+    var avatarUrl: String
+    var name: String?
+    var location: String?
+    var bio: String?
+    var publicRepos: Int
+    var publicGists: Int
+    var htmlUrl: String
+    var following: Int
+    var followers: Int
+    var createdAt: String
+}
