2b047f30ed00a5687a253c314d52979811b73809
Gustavo Tiecker <gustavotiecker@gmail.com>
2020-11-16 22:26:25 -0300

    creating UserInfoVC

---
+++ GithubFollowers/Screens/UserInfoVC.swift
@@ -0,0 +1,16 @@
+//
+//  UserInfoVC.swift
+//  GithubFollowers
+//
+//  Created by Gustavo Tiecker on 16/11/20.
+//
+
+import UIKit
+
+class UserInfoVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+    }
+}
