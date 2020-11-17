04003a7fc0bb691e788ff8f76315cbe84b11c3a9
Gustavo Tiecker <gustavotiecker@gmail.com>
2020-11-17 18:11:25 -0300

    creating child view controller and a new type of label

---
+++ GithubFollowers/Custom Views/ViewControllers/GFUserInfoHeaderVC.swift
@@ -0,0 +1,16 @@
+//
+//  GFUserInfoHeaderVC.swift
+//  GithubFollowers
+//
+//  Created by Gustavo Tiecker on 17/11/20.
+//
+
+import UIKit
+
+class GFUserInfoHeaderVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
