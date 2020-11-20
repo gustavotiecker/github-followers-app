b60799c37c86c4a805b20da8df54f3aa781413ca
Gustavo Tiecker <gustavotiecker@gmail.com>
2020-11-20 16:20:17 -0300

    creating a subclass of GFItemInfoVC

---
+++ GithubFollowers/Custom Views/ViewControllers/GFRepoItemVC.swift
@@ -0,0 +1,19 @@
+//
+//  GFRepoItemVC.swift
+//  GithubFollowers
+//
+//  Created by Gustavo Tiecker on 20/11/20.
+//
+
+import UIKit
+
+class GFRepoItemVC: GFItemInfoVC {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureItems()
+    }
+    
+    private func configureItems() {
+    }
+}
