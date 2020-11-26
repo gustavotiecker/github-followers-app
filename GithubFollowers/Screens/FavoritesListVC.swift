ee5c6ff8875f4b8744e6f734107c8d69b4fe2931
Gustavo Tiecker <gustavotiecker@gmail.com>
2020-11-26 10:55:43 -0300

    implementing the functionality to remove a user from favorites

--- GithubFollowers/Screens/FavoritesListVC.swift
+++ GithubFollowers/Screens/FavoritesListVC.swift
@@ -86,4 +86,19 @@ extension FavoritesListVC: UITableViewDataSource, UITableViewDelegate {
         
         navigationController?.pushViewController(destVC, animated: true)
     }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        guard editingStyle == .delete else { return }
+        
+        let favorite = favorites[indexPath.row]
+        favorites.remove(at: indexPath.row)
+        tableView.deleteRows(at: [indexPath], with: .left)
+        
+        PersistenceManager.updateWith(favorite: favorite, actionType: .remove) { [ weak self ] error in
+            guard let self = self else { return }
+            guard let error = error else { return }
+            
+            self.presentGFAlertONMainThread(title: "Unable to remove", message: error.rawValue, buttonTitle: "Ok")
+        }
+    }
 }
